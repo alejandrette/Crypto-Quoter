e5f0965239b6ee5495527a06e1ce80d1663bdb7c
Alejandrette <alejandrette5117@gmail.com>
2025-03-17 12:34:55 +0100

    create logic fotm

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,61 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { currencies } from "../data/currency";
 
 export function Form() {
+  const [alert, setAlert] = useState('')
+  const [cryptocurrency, setCryptocurrency] = useState({
+    currency: '',
+    crypto: '',
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setCryptocurrency({
+      ...cryptocurrency,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(Object.values(cryptocurrency).includes('')){
+      setAlert('Filed Empty')
+      setTimeout(() => {
+        setAlert('')
+      }, 3000);
+    }
+
+  }
+
   return (
     <div className="w-80 bg-white text-black p-4 rounded-lg m-5">
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        {alert && <p className="bg-red-500 text-center font-bold text-white rounded-lg p-2">{alert}</p>}
+
         <label htmlFor="currency" className="font-bold">Currency</label>
-        <select name="currency" className="bg-transparent text-gray-600">
-          <option value={0}>-- Select --</option>
+        <select 
+          name="currency"
+          className="bg-transparent text-gray-600"
+          value={cryptocurrency.currency}
+          onChange={handleChange}
+        >
+          <option value='' disabled>-- Select --</option>
           {currencies.map(currency => (
             <option key={currency.code} value={currency.code}>{currency.name}</option>
           ))}
         </select>
 
         <label htmlFor="crypto" className="font-bold">Crypto</label>
-        <select name="crypto" className="bg-transparent text-gray-600">
-          <option value={0}>-- Select --</option>
+        <select 
+          name="crypto" 
+          className="bg-transparent text-gray-600"
+          value={cryptocurrency.crypto}  
+          onChange={handleChange}
+        >
+          <option value='' disabled>-- Select --</option>
           {}
         </select>
+
+        <button className="bg-emerald-400 p-2 rounded-lg text-white font-bold">Quote</button>
       </form>
     </div>
   )
